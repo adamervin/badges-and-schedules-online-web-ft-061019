3d1ff7554f8451c3f52ad7a5bb3b3f521e665a9d
Adam Ervin <adamjervin@gmail.com>
2019-06-11 01:19:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name) 
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    badge_maker (name)
+ end
+end
+  
+ def assign_rooms
+   
+   
+ end
